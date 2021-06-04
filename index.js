de12926e70a1358693ad477ce1ad623766174ad1
William Horning <williamhorning@wgyt.tk>
2021-06-04 15:19:57 +0000

    format doc and add rich presence

--- index.js
+++ index.js
@@ -10,33 +10,53 @@ const fetch = require("node-fetch");
 const retronid = require("retronid");
 // auth pages
 app.get("/auth", (req, res) => {
-	res.redirect(`https://scratch.auth.onedot.cf/?url=https://state.onedot.cf/auth/callback`);
+  res.redirect(
+    `https://scratch.auth.onedot.cf/?url=https://state.onedot.cf/auth/callback`
+  );
 });
 app.get("/auth/callback", (req, res) => {
-	var response;
-	if (req.query.verified == 'true') {
-		retroid = retronid.generate();
-		res.send(`<h1>Welcome @${req.query.username}! Enter the following code in your client, but <b><i>DO NOT SHARE IT</i></b>:</h1><br><h2>${retroid}</h2>`)
-		// welcome user
-		user.set(`${req.query.username}`,{online:true,retroid:retroid});
-	} else {
-		res.redirect("/auth");
-	}
+  var response;
+  if (req.query.verified == "true") {
+    retroid = retronid.generate();
+    res.send(
+      `<h1>Welcome @${escape(
+        req.query.username
+      )}! Enter the following code in your client, but <b><i>DO NOT SHARE IT</i></b>:</h1><br><h2>${retroid}</h2>`
+    );
+    // welcome user
+    user.set(`${req.query.username}`, {
+      online: true,
+      retroid: retroid,
+      richpresense: "",
+    });
+  } else {
+    res.redirect("/auth");
+  }
 });
 // api
 app.post("/api/v1/user/:user", async (req, res) => {
-	authtoken = req.body.retroid;
-	username = req.params.user;
-	if ((await auth.get(req.params.user).retroid) === authtoken) {
-		user.set(`${req.params.username}`,{online:req.body.online,retroid:authtoken});
-		res.json({"ok":true});
-	} else {
-		res.json({"ok":false});
-	}
+  authtoken = req.body.retroid;
+  username = req.params.user;
+  if ((await auth.get(req.params.user).retroid) === authtoken) {
+    user.set(`${req.params.username}`, {
+      online: req.body.online,
+      retroid: authtoken,
+      richpresense: req.body.richpresense || "",
+    });
+    res.json({ ok: true });
+  } else {
+    res.json({ ok: false });
+  }
 });
 app.get("/api/v1/user/:user", async (req, res) => {
-	json = await (await fetch(`https://my-ocular.jeffalo.net/api/user/${req.params.user}`)).json();
-	data = { online: (await user.get(req.params.user)).online||false, ocular: json };
-	res.json(data);
+  json = await (
+    await fetch(`https://my-ocular.jeffalo.net/api/user/${req.params.user}`)
+  ).json();
+  data = {
+    online: (await user.get(req.params.user)).online || false,
+    richpresense: (await user.get(req.params.user)).richpresense || "",
+    ocular: json,
+  };
+  res.json(data);
 });
 app.listen(3000);
